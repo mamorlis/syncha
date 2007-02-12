4b2f715dce43cfdd3581b0602dcb3e0d7db54107
Mamoru Komachi <mamoru.komachi@gmail.com>
2007-02-12 01:00:07 +0000

    Backported MyCabocha.pm from mugicha repos.

--- ena/lib/MyCabocha.pm
+++ ena/lib/MyCabocha.pm
@@ -1,4 +1,5 @@
 # Manupulate Extended Cabocha format defined by mamoru-k
+# òµÌ¥ò³ò´
 # Created on 5 July 2006 Mamoru KOMACHI <mamoru-k@is.naist.jp>
 
 =head1 NAME
@@ -18,9 +19,7 @@ my $cab = new MyCabocha <>;
 use strict;
 use warnings;
 
-use Getopt::Std;
-my %opt;
-getopt('d', \%opt);
+#use encoding 'euc-jp';
 
 my $DEBUG = 0;
 
@@ -34,6 +33,9 @@ sub new {
     my $class = shift;
     bless $self, $class;
 
+    # uniq morph id
+    $self->{morph_num} = 0;
+
     my $cabocha_fh = shift;
 
     {
@@ -43,6 +45,8 @@ sub new {
             $self->set_text($i, $texts[$i]);
         }
     }
+
+    $self->parse_arg;
 
     return $self;
 }
@@ -70,7 +74,7 @@ sub set_text {
 
     carp $raw_text if $DEBUG;
 
-    my $text = MyCabocha::Text->new($text_id, $raw_text);
+    my $text = MyCabocha::Text->new(\$self, $text_id, $raw_text);
 
     push @{ $self->{text} }, $text;
 }
@@ -85,11 +89,157 @@ sub get_text {
     return $self->{text};
 }
 
+=item * parse_argstr
+
+=cut
+
+sub parse_arg {
+    my $self = shift;
+
+    my %arg_of;
+
+    for my $text (@{ $self->get_text }) {
+        for my $chunk (@{ $text->get_chunk }) {
+            for my $morph (@{ $chunk->get_morph }) {
+                if (my $id = ($morph->get_argstr =~ m/ID=(\d*)/gmx)[0]) {
+                    $arg_of{$id} = $morph;
+                }
+            }
+        }
+    }
+
+    %{ $self->{arg} } = %arg_of;
+}
+
+=item * get_arg
+
+=cut
+
+sub get_arg {
+    my $self = shift;
+    my $id   = shift;
+
+    $self->{arg}{$id};
+}
+
+=item * get_all_morph
+
+Get all morphs under control.
+
+=cut
+
+sub get_all_morph {
+    my $self = shift;
+ 
+    return map { @{ $_->get_morph } }
+           map { @{ $_->get_chunk } } @{ $self->get_text };
+}
+
+=item * equals
+
+Check if the two objects are the same
+
+=cut
+
+sub equals {
+    my ($self, $obj1, $obj2) = @_;
+
+    if (ref $obj1 eq 'MyCabocha::Morph' and ref $obj2 eq 'MyCabocha::Morph') {
+        if ($obj1->get_id eq $obj2->get_id
+            and $obj1->get_chunk_id eq $obj2->get_chunk_id
+            and $obj1->get_text_id  eq $obj2->get_text_id) {
+            return 1;
+        }
+    } elsif (ref $obj1 eq 'MyCabocha::Chunk' and ref $obj2 eq 'MyCabocha::Chunk') {
+        if ($obj1->get_id eq $obj2->get_id
+            and $obj1->get_text_id eq $obj2->get_text_id) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+=item * before
+
+Check if given object occurs before given object or not.
+
+=cut
+
+sub before {
+    my ($self, $obj1, $obj2) = @_;
+
+    if ($obj1->get_text_id ne $obj2->get_text_id) {
+        warn "Text id is not the same.\n";
+    }
+
+    if (ref $obj1 eq 'MyCabocha::Morph' and ref $obj2 eq 'MyCabocha::Morph') {
+        if ($obj1->get_chunk_id < $obj2->get_chunk_id
+            or $obj1->get_chunk_id == $obj2->get_chunk_id
+            and $obj1->get_id < $obj2->get_id) {
+            return 1;
+        }
+    } elsif (ref $obj1 eq 'MyCabocha::Chunk' and ref $obj2 eq 'MyCabocha::Chunk') {
+        if ($obj1->get_id < $obj2->get_id) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+=item * after
+
+Check if given object occurs after given object or not.
+
+=cut
+
+sub after {
+    my ($self, $obj1, $obj2) = @_;
+
+    if ($obj1->get_text_id ne $obj2->get_text_id) {
+        warn "Text id is not the same.\n";
+    }
+
+    if (ref $obj1 eq 'MyCabocha::Morph' and ref $obj2 eq 'MyCabocha::Morph') {
+        if ($obj1->get_chunk_id > $obj2->get_chunk_id
+            or $obj1->get_chunk_id == $obj2->get_chunk_id
+            and $obj1->get_id > $obj2->get_id) {
+            return 1;
+        }
+    } elsif (ref $obj1 eq 'MyCabocha::Chunk' and ref $obj2 eq 'MyCabocha::Chunk') {
+        if ($obj1->get_id > $obj2->get_id) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+=item * incr_morph_num
+
+=cut
+
+sub incr_morph_num {
+    my $self = shift;
+    $self->{morph_num}++;
+}
+
+=item * get_morph_num
+
+=cut
+
+sub get_morph_num {
+    my $self = shift;
+    return $self->{morph_num};
+}
+
 =head2 MyCabocha::Text
+
+A module to hold all text data for each document.
 
 =cut
 
 package MyCabocha::Text;
+
+#use encoding 'euc-jp';
 
 use Carp;
 use Data::Dumper;
@@ -99,17 +249,20 @@ sub new {
     my $class    = shift;
     bless $self, $class;
 
+    $self->{parent} = shift;
+    $self->char(0);
     my $id       = shift;
     my $raw_data = shift;
 
     $self->set_id($id);
+    $self->in_quote(0);
 
     my @raw_chunks = split /\* /, $raw_data;
     shift @raw_chunks; # empty string
     carp Dumper @raw_chunks if $DEBUG;
     for my $raw_chunk (@raw_chunks) {
         carp ">$raw_chunk<\n" if $DEBUG;
-        my $chunk = new MyCabocha::Chunk($raw_chunk);
+        my $chunk = new MyCabocha::Chunk(\$self, $raw_chunk);
         $self->set_chunk($chunk);
     }
 
@@ -128,6 +281,16 @@ sub puts {
     }
 
     print "EOS\n";
+}
+
+=item * get_surface
+
+=cut
+
+sub get_surface {
+    my $self = shift;
+
+    return map { $_->get_surface } @{ $self->get_chunk };
 }
 
 =item * set_chunk
@@ -173,6 +336,78 @@ sub get_id {
     return $self->{id};
 }
 
+=item * in_quote
+
+Check if in quotation or not.
+
+=cut
+
+sub in_quote {
+    my $self = shift;
+
+    return (@_ > 0) ? $self->{quote} = shift : $self->{quote};
+}
+
+=item * get_np
+
+=cut
+
+sub get_np {
+    my $self = shift;
+
+    my @nps;
+
+    for my $chunk (@{ $self->get_chunk }) {
+        if (my $np_ref = $chunk->get_np) {
+            push @nps, @{ $np_ref };
+        }
+    }
+
+    return \@nps;
+}
+
+=item * get_chunk_by_id
+
+=cut
+
+sub get_chunk_by_id {
+    my $self = shift;
+    my $cid  = shift;
+
+    for my $chunk (@{ $self->get_chunk }) {
+        if ($chunk->get_id == $cid) {
+            return $chunk;
+        }
+    }
+
+    return undef;
+}
+
+=item * get_parent()
+
+=cut
+
+sub get_parent {
+    my $self = shift;
+    return $self->{parent};
+}
+
+=item * char()
+
+Set and get the number of accumulated characters from BOS.
+
+=cut
+
+sub char {
+    my $self = shift;
+    if (@_) {
+        my $len  = shift;
+        $self->{char} += $len;
+    } else {
+        $self->{char};
+    }
+}
+
 =head2 MyCabocha::Chunk
 
 =cut
@@ -187,9 +422,9 @@ sub new {
     my $class     = shift;
     bless $self, $class;
 
-    my $raw_chunk = shift;
+    $self->{parent} = shift;
+    my $raw_chunk   = shift;
     carp ">>$raw_chunk<<\n" if $DEBUG;
-
     my @raw_morphs = split /\n/, $raw_chunk;
 
     # 1¹ÔÌÜ¤Ï¥Ø¥Ã¥À('* %d %d%s %d/%d %f %s\n')
@@ -205,11 +440,80 @@ sub new {
     for (my $i = 0; $i < @raw_morphs; ++$i) {
         last if $raw_morphs[$i] eq 'EOS';
         carp ">>", $raw_morphs[$i], "<<\n" if $DEBUG;
-        my $morph = new MyCabocha::Morph($i, $raw_morphs[$i]);
+        my $morph = new MyCabocha::Morph(\$self, $i, $raw_morphs[$i]);
         $self->set_morph($morph);
+        if ($morph->get_pos =~ m/^Ì¾»ì/gmx) {
+            $self->set_np($morph);
+        }
     }
 
+    # Chunk info
+    if ($self->get_id > 0) {
+        $self->{prev} = \${$self->get_parent}->get_chunk_by_id($self->get_id - 1);
+        ${$self->prev}->next(\$self);
+    }
+
+    # Additional info
+    $self->{quote} = ${$self->get_parent}->in_quote;
+
     return $self;
+}
+
+=item * get_parent
+
+=cut
+
+sub get_parent {
+    my $self = shift;
+    return $self->{parent};
+}
+
+=item * get_text
+
+Get text document.
+
+=cut
+
+sub get_text {
+    my $self = shift;
+    return $self->get_parent;
+}
+
+=item * get_text_id
+
+Get text id.
+
+=cut
+
+sub get_text_id {
+    my $self = shift;
+    return ${$self->get_text}->get_id;
+}
+
+=item * prev
+
+Previous chunk.
+
+=cut
+
+sub prev {
+    my $self = shift;
+    return $self->{prev};
+}
+
+=item * next
+
+Set and get next chunk.
+
+=cut
+
+sub next {
+    my $self = shift;
+    if (@_) {
+        $self->{next} = shift;
+    } else {
+        return $self->{next};
+    }
 }
 
 =item * puts_label
@@ -238,6 +542,16 @@ sub puts {
     for my $morph (@{ $self->get_morph }) {
         $morph->puts;
     }
+}
+
+=item * get_surface
+
+=cut
+
+sub get_surface {
+    my $self = shift;
+
+    return map { $_->get_surface } @{ $self->get_morph };
 }
 
 =item * set_morph
@@ -283,6 +597,18 @@ sub get_id {
     return $self->{id};
 }
 
+=item * in_quote
+
+Check if in quotation or not.
+
+=cut
+
+sub in_quote {
+    my $self = shift;
+
+    $self->{quote};
+}
+
 =item * set_link_rel
 
 =cut
@@ -323,6 +649,45 @@ sub get_head_func {
     return $self->{head_func};
 }
 
+=item * get_head
+
+=cut
+
+sub get_head {
+    my $self = shift;
+
+    my $head_id = (split q{/}, $self->get_head_func)[0];
+    return $self->get_morph_by_id($head_id);
+}
+
+=item * get_func
+
+=cut
+
+sub get_func {
+    my $self = shift;
+
+    my $func_id = (split q{/}, $self->get_head_func)[1];
+    return $self->get_morph_by_id($func_id);
+}
+
+=item * get_morph_by_id
+
+=cut
+
+sub get_morph_by_id {
+    my ($self, $mid) = @_;
+
+    for my $morph (@{ $self->get_morph }) {
+        if ($morph->get_id == $mid) {
+            return $morph;
+        }
+    }
+
+    return undef;
+}
+
+
 =item * set_score
 
 =cut
@@ -343,6 +708,73 @@ sub get_score {
     return $self->{score};
 }
 
+=item * set_np
+
+=cut
+
+sub set_np {
+    my $self  = shift;
+    my $morph = shift;
+
+    push @{ $self->{np} }, $morph;
+}
+
+=item * get_np
+
+=cut
+
+sub get_np {
+    my $self = shift;
+
+    return $self->{np};
+}
+
+=item * get_depend_id
+
+Get depended chunk id.
+
+=cut
+
+sub get_depend_id {
+    my $self = shift;
+
+    my ($cid, $type) = ($self->get_link_rel =~ m/^(-?\d+)([a-zA-Z])$/gmx);
+    return $cid;
+}
+
+=item * get_depend
+
+Get depended chunk.
+
+=cut
+
+sub get_depend {
+    my $self = shift;
+    return ${$self->get_text}->get_chunk_by_id($self->get_depend_id);
+}
+
+=item * get_depend_path
+
+Get depend path.
+
+=cut
+
+sub get_depend_path {
+    my ($self, $paths_ref) = @_;
+
+    my $depend = $self->get_depend;
+    if (!$depend) {
+        return $paths_ref;
+    } else {
+        push @{ $paths_ref }, $depend->get_id;
+        $depend->get_depend_path($paths_ref);
+    }
+}
+
+#
+# set_ and get_ opinion are not intended for general use
+#
+
 sub set_opinion {
     my $self = shift;
 
@@ -361,15 +793,16 @@ sub get_opinion {
 
 package MyCabocha::Morph;
 
+#use encoding 'euc-jp';
+
 sub new {
     my $self      = {};
     my $class     = shift;
     bless $self, $class;
 
-    my $id        = shift;
-    my $raw_morph = shift;
-
-    $self->set_id($id);
+    $self->{parent} = shift;
+    my $id          = shift;
+    my $raw_morph   = shift;
 
     my @labels = split /\t/, $raw_morph;
 
@@ -381,8 +814,162 @@ sub new {
     $self->set_cform (  $labels[5]);
     $self->set_ne(      $labels[6]);
     $self->set_relation($labels[7]);
+    $self->set_argstr(  $labels[8]);
+
+    # extra-morph settings
+    $self->set_num;
+    $self->set_id($id);
+    $self->len(length $self->get_surface);
+    $self->char($self->len);
+
+    my ($head_id, $func_id) = split q[/], ${$self->get_chunk}->get_head_func;
+    $self->is_head(($head_id == $self->get_id) ? 1 : 0);
+    $self->is_func(($func_id == $self->get_id) ? 1 : 0);
+
+    my $text_ref = $self->get_text;
+    if ($self->get_base eq '¡Ö') {
+        ${$text_ref}->in_quote(${$text_ref}->in_quote + 1);
+    }
+    if ($self->get_base eq '¡×') {
+        ${$text_ref}->in_quote(${$text_ref}->in_quote - 1);
+    }
+    $self->{quote} = ${$text_ref}->in_quote;
 
     return $self;
+}
+
+=item * get_parent
+
+=cut
+
+sub get_parent {
+    my $self = shift;
+    return $self->{parent};
+}
+
+=item * get_chunk
+
+Get parent chunk.
+
+=cut
+
+sub get_chunk {
+    my $self = shift;
+    return $self->get_parent;
+}
+
+=item * get_chunk_id
+
+Get chunk id of given morpheme.
+
+=cut
+
+sub get_chunk_id {
+    my $self = shift;
+
+    return ${$self->get_chunk}->get_id;
+}
+
+=item * get_depend_id
+
+Get depended chunk id.
+
+=cut
+
+sub get_depend_id {
+    my $self = shift;
+
+    return ${$self->get_chunk}->get_depend_id;
+}
+
+=item * get_depend
+
+Get depended chunk.
+
+=cut
+
+sub get_depend {
+    my $self = shift;
+
+    return ${$self->get_chunk}->get_depend;
+}
+
+=item * is_head
+
+Return true if the morph is head.
+
+=cut
+
+sub is_head {
+    my $self = shift;
+
+    if (@_ > 0) {
+        $self->{head} = shift;
+    } else {
+        return $self->{head};
+    }
+}
+
+=item * is_func
+
+Return trune if the morph is func.
+
+=cut
+
+sub is_func {
+    my $self = shift;
+
+    if (@_ > 0) {
+        $self->{func} = shift;
+    } else {
+        return $self->{func};
+    }
+}
+
+=item * in_quote
+
+Check if in quotation or not.
+
+=cut
+
+sub in_quote {
+    my $self = shift;
+
+    return $self->{quote};
+}
+
+
+=item * get_cab
+
+Get cab object.
+
+=cut
+
+sub get_cab {
+    my $self = shift;
+    return ${$self->get_text}->get_parent;
+}
+
+=item * get_text
+
+Get parent text.
+
+=cut
+
+sub get_text {
+    my $self = shift;
+    return ${$self->get_chunk}->get_parent;
+}
+
+=item * get_text_id
+
+Get text id.
+
+=cut
+
+sub get_text_id {
+    my $self = shift;
+    return ${$self->get_text}->get_id;
 }
 
 =item * puts
@@ -421,6 +1008,55 @@ sub get_id {
     my $self = shift;
 
     return $self->{id};
+}
+
+=item * set_num
+
+=cut
+
+sub set_num {
+    my $self = shift;
+    my $cab = ${${$self->get_text}->get_parent};
+    $self->{num} = $cab->incr_morph_num - 1;
+}
+
+=item + get_num
+
+=cut
+
+sub get_num {
+    my $self = shift;
+    return $self->{num};
+}
+
+=item * char
+
+Set and get accumulated number of characters from BOS.
+
+=cut
+
+sub char {
+    my $self = shift;
+    if (@_) {
+        my $len  = shift;
+        $self->{char} = ${$self->get_text}->char;
+        ${$self->get_text}->char($len);
+    } else {
+        $self->{char};
+    }
+}
+
+=item * len()
+
+=cut
+
+sub len {
+    my $self = shift;
+    if (@_) {
+        $self->{len} = shift;
+    } else {
+        $self->{len};
+    }
 }
 
 =item * set_surface
@@ -577,6 +1213,82 @@ sub get_relation {
     my $self = shift;
 
     return defined $self->{relation} ? $self->{relation} : '';
+}
+
+=item * set_argstr
+
+=cut
+
+sub set_argstr {
+    my $self = shift;
+
+    $self->{argstr} = $_[0];
+
+    $self->set_type;
+    $self->set_case;
+
+    return $self->{argstr};
+}
+
+=item * get_argstr
+
+=cut
+
+sub get_argstr {
+    my $self = shift;
+
+    return defined $self->{argstr} ? $self->{argstr} : '';
+}
+
+=item * set_type
+
+=cut
+
+sub set_type {
+    my $self = shift;
+
+    if (my $type = ($self->get_argstr =~ m/(EVENT|PRED)/gmx)[0]) {
+        $self->{type} = $type;
+    } else {
+        $self->{type} = '';
+    }
+}
+
+=item * get_type
+
+=cut
+
+sub get_type {
+    my $self = shift;
+
+    $self->{type};
+}
+
+=item * set_case
+
+=cut
+
+sub set_case {
+    my $self = shift;
+
+    for my $case (qw(GA WO NI)) {
+        if (my $ln = ($self->get_argstr =~ m/$case=([^,\s]*)/gmx)[0]) {
+            $self->{$case} = $ln;
+        } else {
+            $self->{$case} = '';
+        }
+    }
+}
+
+=item * get_case
+
+=cut
+
+sub get_case {
+    my $self = shift;
+    my $case = shift;
+
+    $self->{$case};
 }
 
 1;
